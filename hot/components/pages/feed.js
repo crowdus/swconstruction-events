431bc5bf3f02a5e648730ee3096145ec77851917
rrauber <rrauber@uchicago.edu>
2019-11-14 00:14:44 -0600

    event feed pulls from db

--- hot/components/pages/feed.js
+++ hot/components/pages/feed.js
@@ -4,25 +4,18 @@ import {withNavigation} from 'react-navigation';
 
 import Event from '../classes/event';
 
-e = new Event(1, "Event Title", "This will be a very fun event. You should come. I'm going to add more text so we get a new line. Maybe even a third line,,, etc.",
-              new Date("2019-12-17T03:24:00"), new Date("2019-12-17T03:24:00"), "5336 S Greenwood Ave", ["tag1", "tag2", "tag3", "tag4", "tag5", "tag6", "tag7", "antidisestablishmentarianism"], null);
-e1 = new Event(2, "Checkout Books", "This event will have a lot of food. It will be fun. asdf asdf asdf asdf asdf asdf asdf asdf .",
-               new Date("2019-12-17T03:24:00"), new Date("2019-12-17T03:24:00"), "The Reg", ["tag3"], null);
-
-
-
 function TagUI({t}) {
     return (
         <View style={styles.tag_view}><Text style={styles.tag_text}>{t}</Text></View>
     );
 }
 
-
 export default class Feed extends Component {
 
     constructor(props) {
         super(props)
         this.props = props
+        this.state = []
     }
 
     static navigationOptions = {
@@ -49,31 +42,49 @@ export default class Feed extends Component {
         ),
       };
 
+    componentDidMount() {
+
+        fetch('http://hot-backend.herokuapp.com/events/', {
+            method: 'GET',
+        }).then((response) => response.json())
+        .then((responseJson) => {
+            console.log(responseJson)
+            var l = [];
+            for (i in responseJson) {
+                i = responseJson[i]
+                l.push(new Event(i['_id'], i['name'], i['desc'], i['date'], i['date'], i['latitude'], i['tags'], i['admin']));
+                this.setState({data:l})
+            }
+        }).catch((error) => {
+            console.error(error);
+        });
+
+    }
+
     render() {
         const {navigate} = this.props.navigation;
         return(
-            <SafeAreaView>
+            this.state && <SafeAreaView>
                 <FlatList
-                    data={[e, e1]}
+                    data={this.state.data}
                     renderItem={({item}) => 
                         <TouchableOpacity style={styles.evt_card} onPress={function () {navigate('Event', {event:item})}}>
-
-                        <View style={styles.evt_card}>
+                            <View style={styles.evt_card}>
                                 <Text style={styles.evt_title}>{item.get_name()}</Text>
-                                <Text style={styles.evt_date}>{item.get_start_date().toDateString()} - {item.get_end_date().toDateString()}</Text>
+                                <Text style={styles.evt_date}>{item.get_start_date()} - {item.get_end_date()}</Text>
                                 <Text style={styles.evt_addr}>{item.get_address()}</Text>
                                 <Text style={styles.evt_desc}>{item.get_desc()}</Text>
-                            <SafeAreaView style={styles.tags_container}>
-                                <FlatList
-                                    horizontal = {true}
-                                    listKey="tags"
-                                    data={e.get_tags()}
-                                    renderItem={({item}) => <TagUI t={item}/> }
-                                    keyExtractor={item => item}
-                                />
-                            </SafeAreaView>
-                        </View>
-                    </TouchableOpacity>}
+                                <SafeAreaView style={styles.tags_container}>
+                                    <FlatList
+                                        horizontal = {true}
+                                        listKey="tags"
+                                        data={item.get_tags()}
+                                        renderItem={({item}) => <TagUI t={item}/> }
+                                        keyExtractor={item => item}
+                                    />
+                                </SafeAreaView>
+                            </View>
+                        </TouchableOpacity>}
                 />
             </SafeAreaView>
         );
