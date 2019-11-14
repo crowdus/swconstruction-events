f16c296e4b3d1090105153abbbeecf311743309f
rrauber <rrauber@uchicago.edu>
2019-11-14 03:17:33 -0600

    gets all events with tag

--- hot/components/classes/tag.js
+++ hot/components/classes/tag.js
@@ -9,19 +9,29 @@ export default class Tag extends Followable {
 
     constructor(TagID, name) {
         super(TagID, name)
-        this.events = []
+        this.get_events()
         if (!is_valid_name(name) || !is_valid_id(TagID)) {this.name = null; this.ID = null; this.events = null}
     }
 
     get_name() { return this.name }
-    set_name(name) { if (is_valid_name(name)) { this.name = name; return true; } else return false; }
+    //set_name(name) { if (is_valid_name(name)) { this.name = name; return true; } else return false; }
     get_id() { return this.ID }
 
-    render() {
-        return (
-            <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-                <Text>{this.name}</Text>
-            </View>
-        );
+    async get_events() {
+        fetch('http://hot-backend.herokuapp.com/evets/tags/'+this.name, {
+            method: 'GET',
+        }).then((response) => response.json())
+        .then((responseJson) => {
+            var l = [];
+            for (i in responseJson) {
+                i = responseJson[i]
+                l.push(new Event(i['_id'], i['name'], i['desc'], 
+                                 i['start_date'], i['end_date'], 
+                                 i['addr'], i['tags'], i['admins']));
+                return l;
+            }
+        }).catch((error) => {
+            console.error(error);
+        });
     }
 }
