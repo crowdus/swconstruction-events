5766c9b1d84f5c915ca3fcaa69149e69acdc83f0
Lin Jiayi <jiayi@Lins-MacBook-Pro.local>
2019-11-13 18:21:56 -0600

    commit followable

---
+++ hot/components/classes/followable.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+
+export default class Followable extends React.Component{
+  MAX_TAGS = 5
+
+  constructor(props) {
+    super(props)
+  }
+
+  addFollower(user) {}
+  removeFollower(user) {}
+
+}
