338a7c8eb8290f4a7cc9d62626d8c34b98b1e634
Elizabeth Crowdus <ecrowdus@cs.uchicago.edu>
2019-11-13 21:31:52 -0600

    makin moves

--- hot/components/pages/feed.js
+++ hot/components/pages/feed.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { FlatList, StyleSheet, Text, View, SafeAreaView, Header, Button, Icon} from 'react-native';
+import { FlatList, StyleSheet, Text, View, SafeAreaView, Header, Button, Icon, TouchableOpacity} from 'react-native';
+import {withNavigation} from 'react-navigation';
 
 import Event from '../classes/event';
 
@@ -8,68 +9,71 @@ e = new Event(1, "Event Title", "This will be a very fun event. You should come.
 e1 = new Event(2, "Checkout Books", "This event will have a lot of food. It will be fun. asdf asdf asdf asdf asdf asdf asdf asdf .",
                new Date("2019-12-17T03:24:00"), new Date("2019-12-17T03:24:00"), "The Reg", ["tag3"], null);
 
+
+
 function TagUI({t}) {
     return (
         <View style={styles.tag_view}><Text style={styles.tag_text}>{t}</Text></View>
     );
 }
 
-function EventScrollUI({e}) {
-    return (
-        <View style={styles.evt_card}>
-            <Text style={styles.evt_title}>{e.get_name()}</Text>
-            <Text style={styles.evt_date}>{e.get_start_date().toDateString()} - {e.get_end_date().toDateString()}</Text>
-            <Text style={styles.evt_addr}>{e.get_address()}</Text>
-            <Text style={styles.evt_desc}>{e.get_desc()}</Text>
-            <SafeAreaView style={styles.tags_container}>
-                <FlatList
-                    horizontal = {true}
-                    listKey="tags"
-                    data={e.get_tags()}
-                    renderItem={({item}) => <TagUI t={item}/> }
-                    keyExtractor={item => item}
-                />
-            </SafeAreaView>
-        </View>
-    );
-}
-
-
-
 
 export default class Feed extends Component {
+
+    constructor(props) {
+        super(props)
+        this.props = props
+    }
 
     static navigationOptions = {
         headerLeft: () =>  (       
             <Button
-                onPress={() => alert('Take me to list of events nearby that relate to what I\'m following')}
-                title="Explore"
-                color="#ddd"
+                onPress={() => alert('Take me to settings, lists of events im going/interested/admin, following lists')}
+                title="My profile"
+                color="#000"
             />
         ),
         headerTitle: () => (
             <Button
                 onPress={() => alert('Take me to list of events I\'m interested in, going to, or an admin of')}
-                title="View G, I, A"
-                color="#eee"
+                title="Explore"
+                color="#000"
             />
         ),
         headerRight: () => (
             <Button
                 onPress={() => alert('Take me to the create event page')}
                 title="Create event"
-                color="#eee"
+                color="#000"
             />
         ),
       };
 
     render() {
+        const {navigate} = this.props.navigation;
         return(
             <SafeAreaView>
                 <FlatList
                     data={[e, e1]}
-                    renderItem={({item}) => <EventScrollUI e={item}/> }
-                    keyExtractor={item => item.get_name()}
+                    renderItem={({item}) => 
+                        <TouchableOpacity style={styles.evt_card} onPress={function () {navigate('Event', {event:item})}}>
+
+                        <View style={styles.evt_card}>
+                                <Text style={styles.evt_title}>{item.get_name()}</Text>
+                                <Text style={styles.evt_date}>{item.get_start_date().toDateString()} - {item.get_end_date().toDateString()}</Text>
+                                <Text style={styles.evt_addr}>{item.get_address()}</Text>
+                                <Text style={styles.evt_desc}>{item.get_desc()}</Text>
+                            <SafeAreaView style={styles.tags_container}>
+                                <FlatList
+                                    horizontal = {true}
+                                    listKey="tags"
+                                    data={e.get_tags()}
+                                    renderItem={({item}) => <TagUI t={item}/> }
+                                    keyExtractor={item => item}
+                                />
+                            </SafeAreaView>
+                        </View>
+                    </TouchableOpacity>}
                 />
             </SafeAreaView>
         );
