5c0bf29ccd7b643cfa059a0d6315f8ccde561c77
rrauber <rrauber@uchicago.edu>
2019-11-24 18:44:31 -0600

    flex box stuff

--- hot/components/pages/tagsFollowing.js
+++ hot/components/pages/tagsFollowing.js
@@ -1,14 +1,12 @@
 import React, { Component } from 'react';
 import { FlatList, StyleSheet, Text, View, SafeAreaView, Header, Button, TouchableOpacity} from 'react-native';
-import {withNavigation} from 'react-navigation';
 import {NavigationEvents} from "react-navigation";
 import Event from '../classes/event.js';
-import Tag from '../classes/tag.js';
 import TagButton from '../renderables/tagButton.js';
 import User from '../classes/user.js';
 import { DrawerActions } from '@react-navigation/routers';
-import Settings from './settings.js'
-import {NavigationActions} from 'react-navigation';
+import Constants from 'expo-constants';
+
 
 
 var userTA = new User("5dcd241d8a5d632450dea810", "johndoe1234", "John", "Doe", "johndoe@email.com", new Date(), "Password1234", 0, ['am0002'])
@@ -25,51 +23,10 @@ export default class TagsFollowing extends Component {
 
 
 
-    // navigation options displayed at the top of the screen
-    static navigationOptions = ({ navigation }) => {
-        return {
-        headerLeft: () =>  (
-            <Button
-                onPress={() => navigation.dispatch(DrawerActions.toggleDrawer()) }//{navigation.navigate('Drawer')} }
-                title="My profile"
-                color="#000"
-            />
-        ),
-        headerTitle: () => (
-            <Button
-                onPress={() => alert("Iter2: scroll through events that friends are going to, events nearby, and events you\'re interested in.")} //navigation.navigate('UserView)}
-                title="Explore"
-                color="#000"
-            />
-        ),
-        headerRight: () => (
-            <Button
-                onPress={() => navigation.navigate('CreateEvent')}
-                title="Create event"
-                color="#000"
-            />
-        ),
-      };
-    };
-
     // This is called just after the component
     // is first rendered. It changes the data showed there.
     componentDidMount() {
-
-        fetch('http://hot-backend.herokuapp.com/events/', {
-            method: 'GET',
-        }).then((response) => response.json())
-        .then((responseJson) => {
-            var l = [];
-            for (i in responseJson) {
-                i = responseJson[i]
-                l.push(new Event(i['_id'], i['name'], i['desc'], i['start_date'], i['end_date'], i['addr'], i['tags'], i['admins']));
-            }
-            this.setState({data:l})
-        }).catch((error) => {
-            console.error(error);
-        });
-
+        this.setState({data: ['study', 'Study', 'chocolate']});
     }
 
     // the render function!
@@ -80,81 +37,39 @@ export default class TagsFollowing extends Component {
         var usr = this.props.navigation.getParam('usr')
 
         return(
-            this.state && <SafeAreaView>
-                <NavigationEvents onDidFocus={()=>this.componentDidMount()} />
+            this.state && <SafeAreaView style={styles.container}>
+                <NavigationEvents onDidFocus={() => this.componentDidMount()} />
                 <FlatList
                     data={this.state.data}
-                    renderItem={({item}) =>
-                        <TouchableOpacity style={styles.evt_card} onPress={function () {navigate('Event', {evt:item, usr:usr})}}>
-                            <View style={styles.evt_card}>
-                                <Text style={styles.evt_title}>{item.get_name()}</Text>
-                                <Text style={styles.evt_date}>{item.get_start_date().toDateString()} - {item.get_end_date().toDateString()}</Text>
-                                <Text style={styles.evt_addr}>{item.get_address()}</Text>
-                                <Text style={styles.evt_desc}>{item.get_desc()}</Text>
-                                <SafeAreaView style={styles.tags_container}>
-                                    <FlatList
-                                        horizontal = {true}
-                                        listKey="tags"
-                                        data={item.get_tags()}
-                                        renderItem={({item}) => <TagButton t={item} n={this.props.navigation} usr={usr}/> }
-                                        keyExtractor={item => item}
-                                    />
-                                </SafeAreaView>
-                            </View>
-                        </TouchableOpacity>}
+                    renderItem={({item}) => 
+                    <View style={styles.tagContainer}>
+                        <Text style={{alignSelf: 'flex-start', flex:1}}>{item}</Text>
+                        <Text style={styles.followbutton}>Unfollow</Text>
+                    </View>}
                 />
             </SafeAreaView>
         );
     }
 }
 
+{/* <TouchableOpacity style={styles.followbutton} onPress={()=> console.log('unfollow '.concat(item))}> */}
+
+
+
 // styles for the feed.
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        marginTop: 100,
+        flexDirection: 'column',
+        marginTop: Constants.statusBarHeight,
     },
-    header: {
+    followbutton: {
+        flex:1,
+        alignSelf: 'flex-end'
+    },
+    tagContainer: {
         flex: 1,
-        padding:10,
+        flexDirection: 'row',
+        padding: 10
     },
-    tags_container: {
-        flex: 1,
-        padding:10
-    },
-    evt_card: {
-        padding: 10,
-        paddingLeft: 15,
-        marginBottom: 10,
-        backgroundColor: "#eee"
-    },
-    evt_title: {
-        fontSize:32,
-        marginBottom: 5,
-    },
-    evt_date: {
-        color: "#666",
-
-    },
-    evt_addr: {
-        color: "#666",
-        marginBottom: 5,
-
-    },
-    evt_desc: {
-        fontSize: 14,
-        marginBottom: 12,
-    },
-    tag_view: {
-        padding: 5,
-        paddingLeft: 7,
-        marginRight: 5,
-        marginBottom: 5,
-        borderRadius: 2,
-        backgroundColor: "#e5e5e5"
-    },
-    tag_text: {
-        fontSize: 10,
-        color: "#666",
-    }
 });
