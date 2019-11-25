16ab5ed5ff7beea782cc77f6ef85c71b4ab16fed
rrauber <rrauber@uchicago.edu>
2019-11-24 18:19:05 -0600

    search bar

--- hot/App.js
+++ hot/App.js
@@ -5,17 +5,21 @@ const APIKEY = 'AIzaSyB9z1Rab2_34wUVl177HhwEAGa4nh2SnSk'
 import Feed from './components/pages/feed.js';
 import TagButton from './components/renderables/tagButton.js';
 import TagView from './components/pages/tagView.js';
-import LogIn from './components/pages/login.js';
-import CreateEvent from './components/pages/createEvent.js';
+
 import TagsFollowing from './components/pages/tagsFollowing.js';
 import UsersFollowing from './components/pages/usersFollowing.js';
+
 import AdminEvents from './components/pages/adminEvents.js';
 import UpcomingEvents from './components/pages/upcomingEvents.js';
 import PastEvents from './components/pages/pastEvents.js';
 import EventView from './components/pages/eventView.js';
+import CreateEvent from './components/pages/createEvent.js';
+
+import LogIn from './components/pages/login.js';
 import Settings from './components/pages/settings.js'
 import Registration from './components/pages/registration.js'
 import UserView from './components/pages/userView.js'
+import Search from './components/pages/searchView.js'
 
 import {createAppContainer} from 'react-navigation';
 import { createDrawerNavigator } from 'react-navigation-drawer';
@@ -36,6 +40,7 @@ const MainNavigator = createDrawerNavigator({
   LogIn: { screen: LogIn },
 
   Settings: { screen: Settings },
+  Search: { screen: Search },
   Feed: { screen: Feed },
   CreateEvent: { screen: CreateEvent },
   TagsFollowing: { screen: TagsFollowing },
---
+++ hot/components/pages/searchView.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { StyleSheet, Text, TextInput, View, Image } from 'react-native';
+import { SearchBar } from 'react-native-elements';
+import {
+  Button,
+  Alert,
+} from 'react-native';
+import Constants from 'expo-constants';
+
+
+export default class Search extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      search: '',
+    }
+  }
+
+  updateSearch = search => {
+    this.setState({search});
+    console.log(this.state.search)
+  }
+
+  render() {
+    const {search} = this.state;
+    return (
+      <View style={styles.container}>
+      <SearchBar
+        placeholder="Type..."
+        onChangeText={this.updateSearch}
+        value={search}
+      />
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: Constants.statusBarHeight,
+  },
+});
+
+
